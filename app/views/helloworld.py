c75285418b7010f45db9714aa051ab98e799e9d5
xiaoye <zhang.xiaoye@gmail.com>
2017-11-19 14:01:53 +0800

    Move templates into app

--- app/views/helloworld.py
+++ app/views/helloworld.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
-#from app import app
+
+from app import app
 from app.models.demo import Demo
+from flask import render_template
 
 def www_index():
-    return "Hello world!!"
+    return render_template('index.html')
